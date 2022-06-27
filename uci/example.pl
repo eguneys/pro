5f4c2393c6e1749686408b72d7a419ecca12e11b
Emre Guneyler <eguneys@gmail.com>
2022-06-28 00:11:33 +0300

    uci

--- uci/example.pl
+++ uci/example.pl
@@ -1,20 +1,10 @@
-move(Move) --> ["move", Move], { format("__ ~w", Move) }.
-moves(Moves) --> ["moves"], Moves, { length(Moves, 2), format("__ ~w", Moves) }.
+moves([Move]) --> "move ", alnums(Move).
+moves(Moves) --> "moves ", moves_list(Moves).
 
+moves_list([Move|Moves]) --> alnums(Move), " ", moves_list(Moves).
+moves_list([Move]) --> alnums(Move).
 
-% this works as expected.
-hello :- split_string("move ten", " ", "", Ls), phrase(move(_), Ls).
+alnums([C|Cs]) --> alnum(C), alnums(Cs).
+alnums([C]) --> alnum(C).
 
-% this throws arguments error, I want to capture the list [one, two, three].
-hello2 :- split_string("moves one two three", " ", "", Ls), phrase(moves(_), Ls).
-
-
-% finally I want this to work without split_string debacle.
-hello3 :- phrase(move(_), "move ten").
-
-
-% note that it also has to work from strings read from stdin.
-
-hello4 :- 
-read_line_to_string(user_input, L), 
-phrase(move(_), L).
+alnum(C) --> [C].
--- uci/uci.pl
+++ uci/uci.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/env swipl
 
+:- set_prolog_flag(double_quotes, chars).
+
+:- use_module(library(reif)).
 :- use_module(pro2).
 
 
@@ -7,20 +10,20 @@
 
 main() :-
   prompt(_, ''),
+  format("Pro Aid Chess v0.5\n"),
   initial_fen(Fen),
-  uci_loop(go, Fen).
+  uci_loop(Fen).
 
-uci_loop(break, _).
-uci_loop(go, Fen):-
+uci_loop(Fen):-
+  format("here"),
   read_line_to_string(user_input, L),
-  split_string(L, " ", "", Ls),
+  string_chars(L, Ls),
   (phrase(uci(Break, Fen), Ls);
     (phrase(no_match(Break, _), Ls))),
-  uci_loop(Break, Fen).
+  if_(dif(Break, break), uci_loop(Fen), format('bye')).
 
 
-
-go(Fen) --> ["go"], {
+go(Fen) --> "go", {
   fen_board(Fen, TB),
   print_tb(TB),
   format("\n")
@@ -28,16 +31,16 @@ go(Fen) --> ["go"], {
 
 uci(Break, Fen) --> (quit, { Break=break });
 (go(Fen);
-  moves(Moves);
   position(Fen)), { Break=go }.
 
-moves(Moves) --> ["moves"], Moves, { length(Moves, 2), format("moves ~w", Moves) }.
+quit --> "quit".
+position(Fen) --> "position", (start_pos(Fen); custom_fen(Fen)).
 
-quit --> ["quit"].
-position(Fen) --> ["position"], (fen(Fen); custom_fen(Fen)).
+custom_fen(Fen) --> "fen", fen(Fen), { format('here') }.
+start_pos(Fen) --> "startpos", { initial_fen(Fen) }.
 
-fen(Fen) --> ["startpos"], { initial_fen(Fen) }.
-custom_fen(Fen) --> ["fen"], Fen, { length(Fen, 6), format("~a", Fen) }.
+
+fen([Pieses, Turn, Castles, EnPassant, A, B]) --> [Pieses, Turn, Castles, EnPassant, A, B].
 
 no_match(go, []) --> [].
 no_match(go, [X|Xs]) --> [X], no_match(go, Xs).
