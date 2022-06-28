98b895c57d7dd7eff2e339ebed7e3e8df7af2d6b
Emre Guneyler <eguneys@hotmail.com>
2022-06-28 19:48:40 +0000

    fen example thread state

--- uci/example.pl
+++ uci/example.pl
@@ -1,3 +1,5 @@
+:- set_prolog_flag(double_quotes, chars).
+
 moves([Move]) --> "move ", alnums(Move).
 moves(Moves) --> "moves ", moves_list(Moves).
 
@@ -8,3 +10,27 @@ alnums([C|Cs]) --> alnum(C), alnums(Cs).
 alnums([C]) --> alnum(C).
 
 alnum(C) --> [C].
+
+
+state(S), [S] --> [S].
+state(S0, S), [S] --> [S0].
+
+stuff(A) --> state(s(A0), s(A)), {
+    A is A0 + 1
+  }.
+
+hello(A, Ls, B) :- phrase(stuff(B), [s(A)], Ls).
+
+initial_fen(fen).
+next_fen(fen, fen2).
+
+fen(Fen) --> state(s(Fen)), { initial_fen(Fen) }.
+fen2(Fen) --> state(s(Fen0), s(Fen)), { next_fen(Fen0, Fen) }.
+
+world :- 
+read_line_to_string(user_input, Ll),
+atom_string(Ls, Ll),
+((Ls = hello, phrase(fen(Fen), Lq));
+(Ls = world, phrase(fen2(Fen), Lq));
+(Ls = go, format('~a', Fen))),
+world.
--- uci/uci.pl
+++ uci/uci.pl
@@ -14,13 +14,12 @@ main() :-
   initial_fen(Fen),
   uci_loop(Fen).
 
-uci_loop(Fen):-
-  format("here"),
+uci_loop():-
   read_line_to_string(user_input, L),
   string_chars(L, Ls),
-  (phrase(uci(Break, Fen), Ls);
-    (phrase(no_match(Break, _), Ls))),
-  if_(dif(Break, break), uci_loop(Fen), format('bye')).
+  (phrase(uci(s(Break, Fen)), Ls);
+    (phrase(no_match(s(Break, _)), Ls))),
+  if_(dif(Break, break), uci_loop(), format('bye')).
 
 
 go(Fen) --> "go", {
@@ -34,13 +33,16 @@ uci(Break, Fen) --> (quit, { Break=break });
   position(Fen)), { Break=go }.
 
 quit --> "quit".
-position(Fen) --> "position", (start_pos(Fen); custom_fen(Fen)).
+position(Fen) --> "position ", (start_pos(Fen); custom_fen(Fen)).
 
-custom_fen(Fen) --> "fen", fen(Fen), { format('here') }.
-start_pos(Fen) --> "startpos", { initial_fen(Fen) }.
+custom_fen(Fen) --> "fen ", fen(Fen), { format('here') }.
+start_pos(Fen) --> "startpos ", [Fe], { format('hello~a', Fe), initial_fen(Fen) }.
 
 
 fen([Pieses, Turn, Castles, EnPassant, A, B]) --> [Pieses, Turn, Castles, EnPassant, A, B].
 
 no_match(go, []) --> [].
 no_match(go, [X|Xs]) --> [X], no_match(go, Xs).
+
+state(S), [S] --> [S].
+state(S0, S), [S] --> [S0].
